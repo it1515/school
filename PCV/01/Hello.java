47d4f3322e64b5003a8665568de5193cf013906f
student-vyt2.10 <vyt2.10@sspu-opava.cz>
2017-09-18 07:26:58 +0200

    PCV - 01 - Hello.java

--- PCV/01/Hello.java
+++ PCV/01/Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.print("Ciselne soustavy: ");
+        System.out.print("Ciselna soustava: ");
         int x = Integer.parseInt(args[0]);
         System.out.format("HEX: %x%n",x);
     }
