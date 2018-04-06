59b0c373b430b634632928daa9771abaaaa581d2
student-vyt2.10 <vyt2.10@sspu-opava.cz>
2018-04-06 12:23:57 +0200

    PRP - Matice

---
+++ PRP/matice/Matice.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Matice.hpp
+ * Author: student
+ *
+ * Created on 6. dubna 2018, 12:19
+ */
+
+#ifndef MATICE_HPP
+#define MATICE_HPP
+
+class Matice {
+public:
+    Matice();
+    Matice(const Matice& orig);
+    virtual ~Matice();
+private:
+
+};
+
+#endif /* MATICE_HPP */
+
